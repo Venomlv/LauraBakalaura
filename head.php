66e3f7818af018e91e76a4d92b3ec9f625b538e9
Denis-PC\Denis <papa236@inbox.lv>
2018-05-10 22:45:40 +0300

    Added bootstrap and language to head

--- head.php
+++ head.php
@@ -5,9 +5,10 @@
 	<link rel="stylesheet" type="text/css" href="/style.css">
 	<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.0.10/css/all.css" integrity="sha384-+d0P83n9kaQMCwj8F4RJB66tzIwOKmrdb46+porD/OvrJ+37WqIM7UoBtwHO6Nlg" crossorigin="anonymous">
 	<link href="https://fonts.googleapis.com/css?family=Roboto" rel="stylesheet"> 
+	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css" integrity="sha384-WskhaSGFgHYWDcbwN70/dfYBj47jz9qbsMId/iRN3ewGhXQFZCSftd1LZCfmhktB" crossorigin="anonymous">
 	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
 	<script src="/laura.js"></script>
 	<title>Internet veikals</title>
 	<meta name="viewport" content="width=device-width, initial-scale=1">
-	<?php session_start();?>
+	<?php session_start(); $currentlang = isset($_GET['lang'])&&$_GET['lang'] == "ru" ? "?lang=ru" : ""; ?>
 </head>
