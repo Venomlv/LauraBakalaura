ba18516682c70982d6950ad670068aa192cb5432
Denis-PC\Denis <papa236@inbox.lv>
2018-05-10 23:03:36 +0300

    Updated page about and contact

--- about/index.php
+++ about/index.php
@@ -5,27 +5,13 @@
 	require_once($_SERVER["DOCUMENT_ROOT"].'/db.php'); 
 	require_once($_SERVER["DOCUMENT_ROOT"].'/dictionary.php'); 
 	require_once($_SERVER["DOCUMENT_ROOT"].'/Rozes.php'); 
+	require_once($_SERVER["DOCUMENT_ROOT"].'/header.php');
+	require_once($_SERVER["DOCUMENT_ROOT"].'/language.php');
 ?>
 
 <body class="maxSize">
 	<div id="aboutoverlay" class="maxSize"></div>
 	<div id="lending" class="maxSize">
-		<div class="language">
-		<?php echo isset($_GET['lang'])&&$_GET['lang'] == "ru" ? "<a href=\"/about/?lang=lv\">LV</a>" :  "<a href=\"/about/?lang=ru\">RU</a>"; ?>
-		<?php $currentlang = isset($_GET['lang'])&&$_GET['lang'] == "ru" ? "?lang=ru" : ""; ?>
-		</div>
-		<div id="menu">
-			<a class="active" href="<?php echo $href2.''.$currentlang; ?>"><?php echo $about; ?></a>
-			<a href="<?php echo $href3.''.$currentlang; ?>"><?php echo $newrozes; ?></a>
-			<a href="<?php echo $href7.''.$currentlang; ?>"><?php echo $popular; ?></a>
-			<a href="<?php echo $href1.''.$currentlang; ?>"><?php echo $catalog; ?></a>
-			<a href="<?php echo $href4.''.$currentlang; ?>"><?php echo $contacts; ?></a>
-			<?php if(!isset($_SESSION['user'])):?>
-				<a href="<?php echo $href5.''.$currentlang; ?>"><i class="fas fa-user-plus"></i></a>
-			<?php else: ?>
-				<a href="<?php echo $href6.''.$currentlang; ?>"><i class="fas fa-shopping-cart"></i></a>
-			<?php endif; ?>
-		</div>
 		<div id="aboutme">
 			<?php echo $aboutme; ?>
 		</div>
--- contact/index.php
+++ contact/index.php
@@ -5,28 +5,14 @@
 	require_once($_SERVER["DOCUMENT_ROOT"].'/db.php'); 
 	require_once($_SERVER["DOCUMENT_ROOT"].'/dictionary.php'); 
 	require_once($_SERVER["DOCUMENT_ROOT"].'/Rozes.php'); 
+	require_once($_SERVER["DOCUMENT_ROOT"].'/header.php');
+	require_once($_SERVER["DOCUMENT_ROOT"].'/language.php');
 ?>
 
 <body class="maxSize">
 	<div id="contactoverlay" class="maxSize"></div>
 	<div id="lending" class="maxSize">
-		<div class="language">
-		<?php echo isset($_GET['lang'])&&$_GET['lang'] == "ru" ? "<a href=\"/contact/?lang=lv\">LV</a>" :  "<a href=\"/contact/?lang=ru\">RU</a>"; ?>
-		<?php $currentlang = isset($_GET['lang'])&&$_GET['lang'] == "ru" ? "?lang=ru" : ""; ?>
-		</div>
-		<div id="menu">
-			<a href="<?php echo $href2.''.$currentlang; ?>"><?php echo $about; ?></a>
-			<a href="<?php echo $href3.''.$currentlang; ?>"><?php echo $newrozes; ?></a>
-			<a href="<?php echo $href7.''.$currentlang; ?>"><?php echo $popular; ?></a>
-			<a href="<?php echo $href1.''.$currentlang; ?>"><?php echo $catalog; ?></a>
-			<a class="active" href="<?php echo $href4.''.$currentlang; ?>"><?php echo $contacts; ?></a>
-			<?php if(!isset($_SESSION['user'])):?>
-				<a href="<?php echo $href5.''.$currentlang; ?>"><i class="fas fa-user-plus"></i></a>
-			<?php else: ?>
-				<a href="<?php echo $href6.''.$currentlang; ?>"><i class="fas fa-shopping-cart"></i></a>
-			<?php endif; ?>
-		</div>
-		<div id="aboutme">
+		<div id="aboutme" class="mt30pct">
 			<?php echo $mytele; ?>
 		</div>
 	</div>
