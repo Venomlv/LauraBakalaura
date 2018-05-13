a43add5a91a63bcbd55ffd4d9fd63b29bce8e070
Denis-PC\Denis <papa236@inbox.lv>
2018-05-13 13:16:23 +0300

    Was updated login page and css styles

--- login/index.php
+++ login/index.php
@@ -5,27 +5,29 @@
 	require_once($_SERVER["DOCUMENT_ROOT"].'/db.php'); 
 	require_once($_SERVER["DOCUMENT_ROOT"].'/dictionary.php'); 
 	require_once($_SERVER["DOCUMENT_ROOT"].'/User.php'); 
+	require_once($_SERVER["DOCUMENT_ROOT"].'/header.php');
+	require_once($_SERVER["DOCUMENT_ROOT"].'/language.php');
 ?>
 
 <?php 
-	if(isset($_POST['signup']))
+	if(isset($_POST['signup'])) //Функция работает, если человек регистрируется
 	{
 		$email = $_POST['email'];
 		$password = $_POST['password'];
 		$errors = false;
 		
-		if(User::CheckExistEmail($email))
+		if(User::CheckExistEmail($email)) // Эти функции проверяют условия, если не совпадает то записывается в ошибку
 			$errors[] = $kluda1;
 		if(!User::CheckEmail($email))
 			$errors[] = $kluda2;
 		if(!User::CheckPassword($password))
 			$errors[] = $kluda3;
 		
-		if(!$errors)
+		if(!$errors) // Если ошибок нет, то регистрирует
 			User::SignUp($email,$password);
 		
 	}
-	else if(isset($_POST['signin']))
+	else if(isset($_POST['signin'])) // Функция работает, если человек входит
 	{
 		$email = $_POST['email'];
 		$password = $_POST['password'];
@@ -33,11 +35,11 @@
 		
 		if(!User::CheckUserData($email,$password))
 			$errors[] = $kluda4;
-		if(!$errors)
+		if(!$errors) // Если нет ошибок, тогда...
 		{
 			$uid = User::GetUserId($email);
-			$_SESSION['user'] = $uid;
-			header("Location: /");
+			$_SESSION['user'] = $uid; // Создаёт сессию, что человек вошёл
+			header("Location: /"); // Перекидывает его на главную страницу
 		}
 	}
 ?>
@@ -45,37 +47,35 @@
 <body class="maxSize">
 	<div id="overlay" class="maxSize"></div>
 	<div id="lending" class="maxSize">
-		<div class="language">
-		<?php echo isset($_GET['lang'])&&$_GET['lang'] == "ru" ? "<a href=\"/catalog/?lang=lv\">LV</a>" :  "<a href=\"/catalog/?lang=ru\">RU</a>"; ?>
-		<?php $currentlang = isset($_GET['lang'])&&$_GET['lang'] == "ru" ? "?lang=ru" : ""; ?>
-		<?php $inside = isset($_GET['lang'])&&$_GET['lang'] == "ru" ? "&lang=ru" : "&lang=lv"; ?>
-		</div>
-		<div id="menu">
-			<a href="<?php echo $href2.''.$currentlang; ?>"><?php echo $about; ?></a>
-			<a href="<?php echo $href3.''.$currentlang; ?>"><?php echo $newrozes; ?></a>
-			<a href="<?php echo $href7.''.$currentlang; ?>"><?php echo $popular; ?></a>
-			<a class="active" href="<?php echo $href1.''.$currentlang; ?>"><?php echo $catalog; ?></a>
-			<a href="<?php echo $href4.''.$currentlang; ?>"><?php echo $contacts; ?></a>
-			<?php if(!isset($_SESSION['user'])):?>
-				<a href="<?php echo $href5.''.$currentlang; ?>"><i class="fas fa-user-plus"></i></a>
-			<?php else: ?>
-				<a href="<?php echo $href6.''.$currentlang; ?>"><i class="fas fa-shopping-cart"></i></a>
-			<?php endif; ?>
-		</div>
-		<div id="registration">
-				<?php if(isset($_SESSION['user'])):?>
-					<div class="error-info"><?php echo $jauinside; ?></div>
-				<?php else: ?>
-				<h3><?php echo $prereg; ?></h3>
-				<form class="form-control" method="POST">
-					<input class="form-input" name="email" placeholder="E - mail" type="text">
-					<input class="form-input" name="password" placeholder="Password" type="password">
-					<input class="myBtn reg" type="submit" name="signup" value="<?php echo $reg; ?>">
-					<input class="myBtn" type="submit" name="signin" value="<?php echo $log; ?>">
-				</form>
-				<?php endif; ?>
+	
+	<div class="row justify-content-center rozes-view">
+		<div class="col-3">
+			<div class="card">
+				<div class="card-header">
+					<?php echo $log; ?>
+				</div>
+				<div class="card-body">
+					<?php if(isset($_SESSION['user'])):?>
+						<?php echo $jauinside; ?>
+					<?php else: ?>
+						<h6><?php echo $prereg; ?></h6>
+						<form method="POST">
+							<div class="form-group">
+								<input class="form-control" type="text" name="email" placeholder="E - mail">
+								<input class="form-control mgtop15" type="password" name="password" placeholder="Password">
+								<div class="mgtop15 fleft">
+									<button type="submit" class="btn btn-primary" name="signup"><?php echo $reg; ?></button>
+									<button type="submit" class="btn btn-success" name="signin"><?php echo $log; ?></button>
+								</div>
+							</div>
+						</form>
+					<?php endif; ?>
+				</div>
 			</div>
 		</div>
+	</div>
+		
+		<!--Здесь выводятся ошибки, если их нет-->
 		<?php $sc = 20; if(isset($errors)&& is_array($errors)): ?>
 			<?php foreach($errors as $error): ?>
 				<div class="enf noselect" style="top:<?php echo $sc;?>px;"><?php echo $error;?></div>
